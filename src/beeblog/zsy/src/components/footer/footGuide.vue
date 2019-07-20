d109133b40680781e91a83490fcbc503a7c6688f
tmubei <326138915@qq.com>
2019-07-20 23:18:26 +0800

    modify footer

--- src/beeblog/zsy/src/components/footer/footGuide.vue
+++ src/beeblog/zsy/src/components/footer/footGuide.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <ul>
-            <li><a>@2017-2019</a><a style="padding-left:40px;">京ICP备案17049124号-1</a><a style="padding-left:40px;">版权所有</a></li>
+            <li><a>@2017-2019</a><a style="padding:0 20px;">京ICP备案17049124号-1</a><a>版权所有</a></li>
         </ul>
     </footer>
 </template>
